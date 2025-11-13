fde8a8bb35f584897793b1921bbcb284f88393a8
Nakshatraa.d <nakshatrad5075@gmail.com>
2025-11-13 10:53:11 +0530

    Integrate Firebase and handle auth state in main

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,39 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
 import 'package:aura/authentication/login_screen.dart';
+import 'package:aura/screens/home_screen.dart';
 
-
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home:LoginScreen(),
+      title: "AURA",
+      theme: ThemeData(
+        primarySwatch: Colors.purple,
+      ),
+
+      home: StreamBuilder<User?>(
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder: (context, snapshot) {
+
+          if (snapshot.hasData) {
+            return HomeScreen();
+          }
+
+          return LoginScreen();
+        },
+      ),
     );
   }
 }
