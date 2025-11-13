e2a1720b20928ea4180f42e9ea1dff9727927669
Preshal Sharma <118345303+TheBlueCoderr@users.noreply.github.com>
2025-11-13 14:14:38 +0530

    Implement StorageService

---
+++ lib/services/storage_services.dart
@@ -0,0 +1,28 @@
+import 'package:shared_preferences/shared_preferences.dart';
+
+class StorageService {
+  Future<void> saveString(String key, String value) async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setString(key, value);
+  }
+
+  Future<String?> getString(String key) async {
+    final prefs = await SharedPreferences.getInstance();
+    return prefs.getString(key);
+  }
+
+  Future<void> saveBool(String key, bool value) async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setBool(key, value);
+  }
+
+  Future<bool?> getBool(String key) async {
+    final prefs = await SharedPreferences.getInstance();
+    return prefs.getBool(key);
+  }
+
+  Future<void> removeKey(String key) async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.remove(key);
+  }
+}
