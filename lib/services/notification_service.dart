e3b06e46c02183bd5c25a349ea9683e89010b8ce
Preshal Sharma <118345303+TheBlueCoderr@users.noreply.github.com>
2025-11-13 14:12:48 +0530

    Implement NotificationService for notifications

---
+++ lib/services/notification_service.dart
@@ -0,0 +1,40 @@
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+
+class NotificationService {
+  final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+      FlutterLocalNotificationsPlugin();
+
+  Future<void> initializeNotifications() async {
+    const AndroidInitializationSettings androidSettings =
+        AndroidInitializationSettings('@mipmap/ic_launcher');
+
+    const InitializationSettings settings =
+        InitializationSettings(android: androidSettings);
+
+    await flutterLocalNotificationsPlugin.initialize(settings);
+  }
+
+  Future<void> showNotification({
+    required String title,
+    required String body,
+  }) async {
+    
+    const AndroidNotificationDetails androidDetails =
+        AndroidNotificationDetails(
+      'default_channel',
+      'General Notifications',
+      importance: Importance.max,
+      priority: Priority.high,
+    );
+
+    const NotificationDetails details =
+        NotificationDetails(android: androidDetails);
+
+    await flutterLocalNotificationsPlugin.show(
+      0,
+      title,
+      body,
+      details,
+    );
+  }
+}
