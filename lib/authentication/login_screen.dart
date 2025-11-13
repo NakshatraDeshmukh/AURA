99dfaf703b71c0d8b9caa7387f28fb8c8513c2a3
Nakshatraa.d <nakshatrad5075@gmail.com>
2025-11-13 10:45:08 +0530

    Refactor LoginScreen for Firebase authentication

--- lib/authentication/login_screen.dart
+++ lib/authentication/login_screen.dart
@@ -1,136 +1,186 @@
 import 'package:aura/authentication/profile_page.dart';
 import 'package:aura/screens/home_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:aura/authentication/reset_password_screen.dart';
 import 'package:aura/authentication/signup_screen.dart';
 
-class LoginScreen extends StatelessWidget {
-  final TextEditingController _usernameController = TextEditingController();
+class LoginScreen extends StatefulWidget {
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
+  bool _loading = false;
 
-  void _login(BuildContext context) {
-    String username = _usernameController.text;
-    String password = _passwordController.text;
 
-    if (username == 'admin' && password == 'admin') {
-      Navigator.pushReplacement(
-        context,
-        MaterialPageRoute(builder: (context) => ProfilePage()),
+  Future<void> _login(BuildContext context) async {
+    setState(() => _loading = true);
+    String email = _emailController.text.trim();
+    String password = _passwordController.text.trim();
+
+    try {
+
+      // OPTIONAL ADMIN LOGIN
+      if (email == 'admin@gmail.com' && password == 'admin123') {
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(builder: (context) => ProfilePage()),
+        );
+        return;
+      }
+
+      // FIREBASE LOGIN
+      await FirebaseAuth.instance.signInWithEmailAndPassword(
+        email: email,
+        password: password,
       );
-    } else {
+
+      // SUCCESS → GO TO HOME SCREEN
       Navigator.pushReplacement(
         context,
         MaterialPageRoute(builder: (context) => HomeScreen()),
       );
+
+    } on FirebaseAuthException catch (e) {
+      String message = "Login failed";
+
+      if (e.code == 'user-not-found') message = "No user found for this email.";
+      if (e.code == 'wrong-password') message = "Wrong password.";
+      if (e.code == 'invalid-email') message = "Invalid email format.";
+      if (e.code == 'user-disabled') message = "This account has been disabled.";
+
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text(message)),
+      );
+    } finally {
+      setState(() => _loading = false);
     }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        resizeToAvoidBottomInset: true,
-        backgroundColor: Colors.black87,
-        body: SingleChildScrollView(
-            child: Center(
-              child: Padding(
-                padding: EdgeInsets.all(16.0),
-                child: SizedBox(
-                  height: MediaQuery.of(context).size.height,
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    mainAxisSize: MainAxisSize.min,
+      resizeToAvoidBottomInset: true,
+      backgroundColor: Colors.black87,
+      body: SingleChildScrollView(
+        child: Center(
+          child: Padding(
+            padding: EdgeInsets.all(16.0),
+            child: SizedBox(
+              height: MediaQuery.of(context).size.height,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  CircleAvatar(
+                    radius: 50,
+                    backgroundImage: AssetImage('assets/icons/logo.jpg'),
+                  ),
+                  SizedBox(height: 20),
+
+                  Text(
+                    'Welcome Back',
+                    style: TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                  ),
+                  SizedBox(height: 8),
+
+                  Text(
+                    'Sign in to continue to your account',
+                    style: TextStyle(color: Colors.white70),
+                  ),
+                  SizedBox(height: 20),
+
+                  _buildTextField(Icons.email, 'Email Address',
+                      controller: _emailController),
+                  SizedBox(height: 10),
+
+                  _buildTextField(Icons.lock, 'Password',
+                      obscureText: true, controller: _passwordController),
+
+                  SizedBox(height: 20),
+
+                  _loading
+                      ? CircularProgressIndicator(color: Colors.white)
+                      : ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: Color(0xFF9E3581),
+                            padding: EdgeInsets.symmetric(
+                                horizontal: 50, vertical: 12),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(8.0),
+                            ),
+                          ),
+                          onPressed: () => _login(context),
+                          child: Text(
+                            'Sign In',
+                            style: TextStyle(
+                                fontSize: 16, color: Colors.white),
+                          ),
+                        ),
+
+                  SizedBox(height: 10),
+
+                  Row(
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: [
-                      CircleAvatar(
-                        radius: 50,
-                        backgroundImage: AssetImage('assets/icons/logo.jpg'),
-                      ),
-                      SizedBox(height: 20),
-                      Text(
-                        'Welcome Back',
-                        style: TextStyle(
-                          fontSize: 24,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.white,
-                        ),
-                      ),
-                      SizedBox(height: 8),
-                      Text(
-                        'Sign in to continue to your account',
-                        style: TextStyle(color: Colors.white70),
-                      ),
-                      SizedBox(height: 20),
-                      _buildTextField(Icons.email, 'Email Address',
-                          controller: _usernameController),
-                      SizedBox(height: 10),
-                      _buildTextField(Icons.lock, 'Password',
-                          obscureText: true, controller: _passwordController),
-                      SizedBox(height: 20),
-                      ElevatedButton(
-                        style: ElevatedButton.styleFrom(
-                          backgroundColor: Color(0xFF9E3581),
-                          padding:
-                          EdgeInsets.symmetric(horizontal: 50, vertical: 12),
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(8.0),
-                          ),
-                        ),
-                        onPressed: () => _login(context),
-                        child: Text(
-                          'Sign In',
-                          style: TextStyle(fontSize: 16, color: Colors.white),
-                        ),
-                      ),
-                      SizedBox(height: 10),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Text("Don't have an account? ",
-                              style: TextStyle(color: Colors.white70)),
-                          GestureDetector(
-                            onTap: () {
-                              Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                    builder: (context) =>
-                                        SignUpScreen()), // Directly go to SignUpScreen
-                              );
-                            },
-                            child: Text('Sign Up',
-                                style: TextStyle(color: Color(0xFF9E3581))),
-                          ),
-                        ],
-                      ),
-                      SizedBox(height: 5),
+                      Text("Don't have an account? ",
+                          style: TextStyle(color: Colors.white70)),
                       GestureDetector(
                         onTap: () {
                           Navigator.push(
                             context,
                             MaterialPageRoute(
-                                builder: (context) => ResetPasswordScreen()
-                            ),
+                                builder: (context) => SignUpScreen()),
                           );
                         },
-                        child: Text('Forgot Password?',
+                        child: Text('Sign Up',
                             style: TextStyle(color: Color(0xFF9E3581))),
-                      ),
-                      SizedBox(height: 20),
-                      Text('Or continue with',
-                          style: TextStyle(color: Colors.white70)),
-                      SizedBox(height: 10),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          _buildSocialButton('Google', Icons.g_mobiledata),
-                          SizedBox(width: 10),
-                          _buildSocialButton('Apple', Icons.apple),
-                        ],
                       ),
                     ],
                   ),
-                ),
+
+                  SizedBox(height: 5),
+
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => ResetPasswordScreen()),
+                      );
+                    },
+                    child: Text('Forgot Password?',
+                        style: TextStyle(color: Color(0xFF9E3581))),
+                  ),
+
+                  SizedBox(height: 20),
+
+                  Text('Or continue with',
+                      style: TextStyle(color: Colors.white70)),
+                  SizedBox(height: 10),
+
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      _buildSocialButton('Google', Icons.g_mobiledata),
+                      SizedBox(width: 10),
+                      _buildSocialButton('Apple', Icons.apple),
+                    ],
+                  ),
+                ],
               ),
-            )));
+            ),
+          ),
+        ),
+      ),
+    );
   }
 
   Widget _buildTextField(IconData icon, String hintText,
